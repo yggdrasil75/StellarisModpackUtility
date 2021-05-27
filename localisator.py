b98f63a3c4c25226a1b4cf8b9ae40022fa9f1237
F1r3Pr1nc3 <perhelion3@gmail.com>
2021-05-27 13:44:19 +0200

    + modupdater3.0

--- localisator.py
+++ localisator.py
@@ -1,61 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-## USAGE: You need install https://pyyaml.org/wiki/PyYAMLDocumentation for Python3.x
-## ATTENTION: You must customize the vars localModPath and local_OVERHAUL
-## TODO: Renaming (already translated) keys is not working
+# USAGE: You need install https://pyyaml.org/wiki/PyYAMLDocumentation for Python3.x
+# ATTENTION: You must customize the vars localModPath and local_OVERHAUL
+# TODO: Renaming (already translated) keys is not working
 import os
 import io
 import tkinter as tk
 from tkinter import messagebox
-import traceback
+# import traceback
 # import sys
 # import json
 # from ruamel.yaml import YAML
 # from ruamel.yaml.compat import StringIO
-import yaml
 import re
 import glob
+import yaml
 # yaml=YAML(typ='safe')
 
 # Write here your mod folder name
 # localModPath = "ADeadlyTempest"
-# localModPath = "Decentralized Empires"
-localModPath = "starbasestrong"
-localizations = ["english", "german", "russian", "spanish", "braz_por", "french", "polish", "simp_chinese"]
-local_OVERHAUL = ["russian", "braz_por", "french", "polish", "simp_chinese"]
+localModPath = "Decentralized Empires"
+# localModPath = "starbasestrong"
+local_OVERHAUL = ["russian", "spanish",
+                  "braz_por", "french", "polish", "simp_chinese"]
 print(localModPath)
 
-# localizations = ["english", "russian"]
+# localizations = ["english", "russian", "spanish"]
 
 # def abort(message):
 # 	mBox('abort', message, 0)
 # 	sys.exit(1)
 
 
-def mBox(type, text):
-	tk.Tk().withdraw()
-	style = not type and messagebox.showinfo or type == 'Abort' and messagebox.showwarning or messagebox.showerror
-	style(title=type, message=text)
+def mBox(mtype, text):
+    tk.Tk().withdraw()
+    style = not mtype and messagebox.showinfo or mtype == 'Abort' and messagebox.showwarning or messagebox.showerror
+    style(title=mtype, message=text)
 
 
-def iBox(title, prefil, master):
-	answer = filedialog.askdirectory(
-		initialdir=prefil,
-		title=title,
-		parent=master)
-	return answer
+def iBox(title, prefil): # , master
+    answer = tk.filedialog.askdirectory(
+        initialdir=prefil,
+        title=title,
+        # parent=master
+        )
+    return answer
 
-# mods_registry = "mods_registry.json" # old launcher (changed in 2.7.2) 
+
+# mods_registry = "mods_registry.json" # old launcher (changed in 2.7.2)
 mods_registry = "settings.txt"
+localizations = ["english", "german", "russian", "spanish",
+                 "braz_por", "french", "polish", "simp_chinese"]
 
 # Check Stellaris settings location
 settingsPath = [
-	".", "..",
-	os.path.join(os.path.expanduser('~'), 'Documents', 'Paradox Interactive', 'Stellaris'),
-	os.path.join(os.path.expanduser('~'), '.local', 'share', 'Paradox Interactive', 'Stellaris')
+    ".", "..",
+    os.path.join(os.path.expanduser('~'), 'Documents',
+                 'Paradox Interactive', 'Stellaris'),
+    os.path.join(os.path.expanduser('~'), '.local', 'share',
+                 'Paradox Interactive', 'Stellaris')
 ]
 
-settingsPath = [s for s in settingsPath if os.path.isfile(os.path.join(s, mods_registry))]
+settingsPath = [s for s in settingsPath if os.path.isfile(
+    os.path.join(s, mods_registry))]
 # for s in settingsPath:
 # 	if os.path.isfile(os.path.join(s, mods_registry)):
 # 		settingsPath[0] = s
@@ -63,12 +70,12 @@ settingsPath = [s for s in settingsPath if os.path.isfile(os.path.join(s, mods_r
 print(settingsPath)
 
 if len(settingsPath):
-	settingsPath = settingsPath[0]
+    settingsPath = settingsPath[0]
 else:
-	from tkinter import filedialog
-	mBox('Error', 'Unable to locate ' + mods_registry)
-	settingsPath = iBox(
-		"Please select the Stellaris settings folder:", settingsPath[0])
+    # from tkinter import filedialog
+    mBox('Error', 'Unable to locate ' + mods_registry)
+    settingsPath = iBox(
+        "Please select the Stellaris settings folder:", settingsPath[0])
 
 # mods_registry = os.path.join(settingsPath, mods_registry)
 
@@ -76,64 +83,69 @@ localModPath = os.path.join(settingsPath, "mod", localModPath, "localisation")
 
 os.chdir(localModPath)
 
-regRev1 =re.compile(r'^ +\"([^:"\s]+)\": ', re.MULTILINE)
+regRev1 = re.compile(r'^ +\"([^:"\s]+)\": ', re.MULTILINE)
 regRev2 = re.compile(r'(?:\'|([^:"]{2}))\'?$', re.MULTILINE)
 
+
 def tr(s):
-	print(type(s),len(s))
-	if type(s) is bytes: s = s.decode('utf-8-sig')
-	# s = re.sub('\n', '\\n', s)
-	s = s.replace('\\r?\\n', 'BRR')
-	# s = s.replace("\"", '”')
-	s = s.replace("\'", '’')
-	# s = s.replace(":", '…')
-	# s = re.sub(r'\\n', '\\n', s)
-	return re.sub(r':[0-2] ', ': ', s)
+    print(type(s), len(s))
+    if isinstance(s) is bytes:
+        s = s.decode('utf-8-sig')
+    # s = re.sub('\n', '\\n', s)
+    s = s.replace('\\r?\\n', 'BRR')
+    # s = s.replace("\"", '”')
+    s = s.replace("\'", '’')
+    # s = s.replace(":", '…')
+    # s = re.sub(r'\\n', '\\n', s)
+    return re.sub(r':[0-2] ', ': ', s)
 
 
 def trReverse(s):
-	"Paradox workaround"
-	print(type(s))
-	if type(s) is bytes: s = s.decode('utf-8-sig')
-	s = s.replace('\r\n', '\n') # Windows
-	s = s.replace('  ', ' ')
-	s = re.sub(r'BRR *', r'\\n', s)
-	s = re.sub(regRev1, r' \g<1>:0 ', s) # add 0 to keys
-	s = re.sub(re.compile(r'^"(l_\S+)":\n'), r'\1:\n', s)
-	# s = s.replace("”", "\"")
-	s = s.replace("’", "\'")
-	# s = s.replace("…", ':')
-	# s = re.sub(regRev2, r'\1"', s)
-	return s
+    "Paradox workaround"
+    print(type(s))
+    if isinstance(s) is bytes:
+        s = s.decode('utf-8-sig')
+    s = s.replace('\r\n', '\n')  # Windows
+    s = s.replace('  ', ' ')
+    s = re.sub(r'BRR *', r'\\n', s)
+    s = re.sub(regRev1, r' \g<1>:0 ', s)  # add 0 to keys
+    s = re.sub(re.compile(r'^"(l_\S+)":\n'), r'\1:\n', s)
+    # s = s.replace("”", "\"")
+    s = s.replace("’", "\'")
+    # s = s.replace("…", ':')
+    # s = re.sub(regRev2, r'\1"', s)
+    return s
 
 
 def getYAMLstream(lang, filename):
-	"Read YAML file"
-	if lang != "english":
-		filename = filename.replace("english", lang)
-	lang = os.path.join(os.getcwd(), filename)
-	# print(lang)
-	if os.path.isfile(lang):
-		return io.open(lang, "rb") # "rb" , encoding='utf-8-sig'
+    "Read YAML file"
+    if lang != "english":
+        filename = filename.replace("english", lang)
+    lang = os.path.join(os.getcwd(), filename)
+    # print(lang)
+    if os.path.isfile(lang):
+        return io.open(lang, "rb")  # "rb" , encoding='utf-8-sig'
 
 
 def writeStream(lang, stream, filename):
-	"Write YAML file"
-	filename = filename.replace("english", lang)
-	if not os.path.isdir(lang):
-		try:
-			os.mkdir(lang)
-		except OSError:
-			print ("Creation of the directory %s failed" % lang)
-		else:
-			print ("Successfully created the directory %s " % lang)
-	lang = os.path.join(os.getcwd(), filename)
-	print(lang, os.path.isfile(lang))
-	# if not os.path.isfile(lang):
-	if type(stream) is bytes: stream = stream.decode('utf-8-sig')
-	with io.open(lang, 'w', encoding='utf-8-sig') as f:
-		f.write(stream)
-		# yaml.dump(stream, f, indent=1)
+    "Write YAML file"
+    filename = filename.replace("english", lang)
+    if not os.path.isdir(lang):
+        try:
+            os.mkdir(lang)
+        except OSError:
+            print("Creation of the directory %s failed" % lang)
+        else:
+            print("Successfully created the directory %s " % lang)
+    lang = os.path.join(os.getcwd(), filename)
+    print(lang, os.path.isfile(lang))
+    # if not os.path.isfile(lang):
+    if isinstance(stream) is bytes:
+        stream = stream.decode('utf-8-sig')
+    with io.open(lang, 'w', encoding='utf-8-sig') as f:
+        f.write(stream)
+        # yaml.dump(stream, f, indent=1)
+
 
 # yaml = ruamel.yaml.YAML(typ='safe')
 yaml.default_flow_style = False
@@ -145,74 +157,78 @@ yaml.allow_unicode = True
 
 #CrisisManagerEvent_l_english ,'**'
 for filename in glob.iglob(os.path.join('english', '*.yml'), recursive=False):
-	print(filename)
-	streamEn = getYAMLstream(localizations[0], filename)
-	streamEn = streamEn.read()
-	# print(streamEn)
-	dictionary = {}
-	# try:
-	# 	print(type(dictionary),dictionary)
-	# 	# print(dictionary["ï»¿l_english"])
-	# except yaml.YAMLError as exc:
-	# 	print(exc)
-	# doc = yaml.load_all(stream, Loader=yaml.FullLoader)
-	# doc = yaml.dump(dictionary) # ["\u00ef\u00bb\u00bfl_english"]
-	# doc = json.dumps(dictionary) # ["\u00ef\u00bb\u00bfl_english"]
-	# doc = yaml.dump(dictionary)
-	# print(type(dictionary), dictionary)
-	# doc = tr(dictionary['l_english'])
-	# dictionary = yaml.load(tr(streamEn), Loader=yaml.FullLoader)
-	dictionary = yaml.safe_load(tr(streamEn))
-	# print("New document:", type(dictionary))
-	doc = dictionary["l_english"]
-	# print(type(doc), doc)
-	# for doc in dictionary:
-	for lang in range(1, len(localizations)):
-		changed = False
-		lang = localizations[lang]
-		stream = getYAMLstream(lang, filename)
-		if not stream:
-			stream = {}
-			print("Create new document "+lang)
-			stream = streamEn.replace(b'l_english', bytes('l_'+lang, "utf-8"))
-			# copy file with new header
-			writeStream(lang, stream, filename)
-			continue
+    print(filename)
+    streamEn = getYAMLstream(localizations[0], filename)
+    streamEn = streamEn.read()
+    # print(streamEn)
+    dictionary = {}
+    # try:
+    # 	print(type(dictionary),dictionary)
+    # 	# print(dictionary["ï»¿l_english"])
+    # except yaml.YAMLError as exc:
+    # 	print(exc)
+    # doc = yaml.load_all(stream, Loader=yaml.FullLoader)
+    # doc = yaml.dump(dictionary) # ["\u00ef\u00bb\u00bfl_english"]
+    # doc = json.dumps(dictionary) # ["\u00ef\u00bb\u00bfl_english"]
+    # doc = yaml.dump(dictionary)
+    # print(type(dictionary), dictionary)
+    # doc = tr(dictionary['l_english'])
+    # dictionary = yaml.load(tr(streamEn), Loader=yaml.FullLoader)
+    dictionary = yaml.safe_load(tr(streamEn))
+    # print("New document:", type(dictionary))
+    doc = dictionary["l_english"]
+    # print(type(doc), doc)
+    # for doc in dictionary:
+    for lang in range(1, len(localizations)):
+        changed = False
+        lang = localizations[lang]
+        stream = getYAMLstream(lang, filename)
+        if not stream:
+            stream = {}
+            print("Create new document "+lang)
+            stream = streamEn.replace(b'l_english', bytes('l_'+lang, "utf-8"))
+            # copy file with new header
+            writeStream(lang, stream, filename)
+            continue
 
-		langStream = tr(stream.read())
-		# print("Str document:", type(langStream), langStream)
-		# langStream = yaml.load(langStream, Loader=yaml.FullLoader)
-		langStream = yaml.safe_load(langStream)
+        langStream = tr(stream.read())
+        # print("Str document:", type(langStream), langStream)
+        # langStream = yaml.load(langStream, Loader=yaml.FullLoader)
+        langStream = yaml.safe_load(langStream)
 
-		if not "l_"+lang in langStream:
-			print("FAIL on file", filename.replace("english", lang), langStream)
-			continue
-		langDict = langStream["l_"+lang]
-		#print("Dict document:", type(langStream), langStream)
+        if not "l_"+lang in langStream:
+            print("FAIL on file", filename.replace(
+                "english", lang), langStream)
+            continue
+        langDict = langStream["l_"+lang]
+        #print("Dict document:", type(langStream), langStream)
 
-		# for _, doc in dictionary.items():
-		if type(doc) is dict and type(langDict) is dict:
-			for key, value in doc.items():
-				# print(key, value)
-				if key not in langDict or (lang in local_OVERHAUL and langDict[key] != value):
-					langDict[key] = value
-					changed = True
-					print("Fixed document " + filename.replace("english", lang), key, value)
-					# break
-				# else: print(bytes(key + ":0 " + langDict[key], "utf-8").decode("utf-8"))
-			for key in list(langDict.keys()):
-				if key not in doc:
-					del langDict[key]
-					changed = True
-					print(key, "removed from document " + filename.replace("english", lang))
+        # for _, doc in dictionary.items():
+        if isinstance(doc) is dict and isinstance(langDict) is dict:
+            for key, value in doc.items():
+                # print(key, value)
+                if key not in langDict or (lang in local_OVERHAUL and langDict[key] != value):
+                    langDict[key] = value
+                    changed = True
+                    print("Fixed document " +
+                          filename.replace("english", lang), key, value)
+                    # break
+                # else: print(bytes(key + ":0 " + langDict[key], "utf-8").decode("utf-8"))
+            for key in list(langDict.keys()):
+                if key not in doc:
+                    del langDict[key]
+                    changed = True
+                    print(key, "removed from document " +
+                          filename.replace("english", lang))
 
-		if changed:
-			# dictionary = doc.copy()
-			# dictionary.update(langDict)
-			# langStream["l_"+lang] = dictionary
-			langStream["l_"+lang] = langDict
-			# print(type(langStream), langStream)
-			langStream = yaml.dump(langStream, width=10000, allow_unicode=True, indent=1, default_style='"') # , encoding='utf-8'
-			langStream = trReverse(langStream)
-			# print(type(langStream), langStream.encode("utf-8"))
-			writeStream(lang, langStream, filename)
+        if changed:
+            # dictionary = doc.copy()
+            # dictionary.update(langDict)
+            # langStream["l_"+lang] = dictionary
+            langStream["l_"+lang] = langDict
+            # print(type(langStream), langStream)
+            langStream = yaml.dump(langStream, width=10000, allow_unicode=True,
+                                   indent=1, default_style='"')  # , encoding='utf-8'
+            langStream = trReverse(langStream)
+            # print(type(langStream), langStream.encode("utf-8"))
+            writeStream(lang, langStream, filename)
---
+++ modupdater3.0.py
@@ -0,0 +1,140 @@
+
+import os  # io for high level usage
+import glob
+import re
+# from pathlib import Path
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+mod_path = 'c:/Games/Settings/Mods/CrisisManagers (all)'
+mod_outpath = mod_path
+
+# 3.0.*
+removedTargets = {
+    "produced_energy",
+    "trade_expenses",
+    "ship_maintenance",
+    "army_maintenance",
+    "colony_maintenance",
+    "station_maintenance",
+    "construction_expenses",
+    "federation_expenses"
+}
+# 3.0.*
+targets3 = {
+    r"surveyed\s*=\s*\{(\s*)set_surveyed": r"set_surveyed = {\1surveyed",
+    r"planet = solar_system\n?": "",
+    "has_completed_special_project": "has_completed_special_project_in_log",
+    "has_failed_special_project": "has_failed_special_project_in_log",
+    r"species = last_created(\s)": r"species = last_created_species\1",
+    r"owner = last_created(\s)": r"owner = last_created_country\1",
+    r"(any|every|random)_pop\s*=": r"\1_owned_pop =",
+    r"(\s)(any|every|random)_planet\s*=": r"\1\2_galaxy_planet =",
+    r"(\s)(any|every|random)_ship\s*=": r"\1\2_galaxy_fleet =",
+    r"(any|every|random|count)_sector\s*=": r"\1_galaxy_sector =",
+    r"(any|every|random)_war_defender\s*=": r"\1_defender =",
+    r"(any|every|random)_war_attacker\s*=": r"\1_attacker =",
+    r"(any|every|random|count)_recruited_leader\s*=": r"\1_owned_leader =",
+    "count_planets ": "count_galaxy_planet ",
+    "count_ships ": "count_galaxy_fleet ",
+    "count(_owned)?_pops ": "count_owned_pop ",
+    "count_owned_ships ": "count_owned_ship ",
+    "any_ship_in_system": "any_fleet_in_system",
+    r"spawn_megastructure = \{\n([^{}]+)location": r"spawn_megastructure = {\n\1planet",
+    r"any_system_within_border\s*=\s*\{[\s\n]*any_system_planet\s*=": "any_planet_within_border =",
+    r"is_country_type = default\s+has_monthly_income = \{ resource = (\w+) value <=? \d": r"no_resource_for_component = { RESOURCE = \1",
+    r"([^\._])(?:space_)?owner\s*=\s*\{\s*is_(?:same_empire|country|same_value)\s*=\s*(\w+)\s*\}": r"\1is_owned_by = \2",
+}
+
+
+def mBox(mtype, text):
+    tk.Tk().withdraw()
+    style = not mtype and messagebox.showinfo or mtype == 'Abort' and messagebox.showwarning or messagebox.showerror
+    style(title=mtype, message=text)
+
+
+def iBox(title, prefil):  # , master
+    answer = filedialog.askdirectory(
+        initialdir=prefil,
+        title=title,
+        # parent=master
+    )
+    return answer
+
+
+def parse_dir():
+    global mod_path, mod_outpath
+    files = []
+    mod_path = os.path.normpath(mod_path)
+    mod_path = iBox("Please select a mod folder:", mod_path)
+    # mod_path = input('Enter target directory: ')
+    mod_outpath = iBox('Enter out directory (optional):', mod_path)
+    # mod_outpath = input('Enter out directory (optional):')
+    mod_outpath = os.path.normpath(mod_outpath)
+
+    if not os.path.isdir(mod_path):
+        # except OSError:
+        #     print('Unable to locate the mod path %s' % mod_path)
+        mBox('Error', 'Unable to locate the mod path %s' % mod_path)
+    if not os.path.isdir(mod_outpath) or mod_outpath == mod_path:
+        mod_outpath = mod_path
+        print('Warning: Mod files will be overwritten!')
+    else: mod_outpath = os.path.normpath(mod_outpath)
+
+    files += glob.glob(mod_path + '/**', recursive=True)  # '\\*.txt'
+    # files = glob.glob(mod_path, recursive=True)  # os.path.join( ,'\\*.txt'
+
+    modfix(files)
+
+
+def modfix(file_list):
+    # print(targets3)
+    # print(mod_outpath)
+    # print(mod_path)
+    subfolder = ''
+    for _file in file_list:
+        if os.path.isfile(_file) and re.search(r"\.txt$", _file):
+            file_contents = ""
+            with open(_file, 'r', encoding='utf-8') as txtfile:
+                # try:
+                # print(_file, type(_file))
+                file_contents = txtfile.readlines()
+                basename = os.path.basename(_file)
+
+                txtfile.close()
+                out = ""
+                # changed = False
+                for i, line in enumerate(file_contents):
+                # for line in file_contents:
+                    # print(line)
+                    for rt in removedTargets:
+                        if rt in line:
+                            print("WARNING outdated removed trigger: %s in line %i file %s" % (rt, i, basename))
+                            # changed = True
+                    for pattern, repl in targets3.items():
+                        if re.search(pattern, line):
+                            # , count=0, flags=0
+                            line = re.sub(pattern, repl, line)
+                            # line = line.replace(t, r)
+                            print("Updated file: %s at line %i with %s" % (basename, i, line.strip()))
+                            # changed = True
+
+                    out += line
+                out_file = os.path.join(mod_outpath, subfolder, basename)
+                # if changed: print('Changed file', out_file)
+                with open(out_file, "w", encoding='utf-8') as txtfile:
+                    txtfile.write(out)
+                # except Exception as e:
+                # except OSError as e:
+                #     print(e)
+                #     print("Unable to open", _file)
+            txtfile.close()
+        elif os.path.isdir(_file):
+            # if _file.is_dir():
+            subfolder = os.path.basename(_file)
+            # print("subfolder:", subfolder)
+        # else: print("NO TXT?", _file)
+    print("Done!")
+
+parse_dir() #mod_path, mod_outpath
