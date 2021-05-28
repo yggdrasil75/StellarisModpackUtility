c854e1a070515d4a0633b4f4a9da8506672a1f99
F1r3Pr1nc3 <perhelion3@gmail.com>
2021-05-28 10:56:01 +0200

    Update modupdater3.0.py
    improved

--- modupdater3.0.py
+++ modupdater3.0.py
@@ -7,29 +7,25 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 
-mod_path = os.path.expanduser('~') + '\Documents\Paradox Interactive\Stellaris\mod'
+mod_path = os.path.expanduser('~') + '/Documents/Paradox Interactive/Stellaris/mod'
 # mod_path = 'c:/Games/Settings/Mods/CrisisManagers (all)'
+if not os.path.isdir(mod_path):
+    mod_path = os.getcwd()
 mod_outpath = mod_path
 
 # 3.0.*
 removedTargets = {
-    "produced_energy",
-    "trade_expenses",
-    "ship_maintenance",
-    "army_maintenance",
-    "colony_maintenance",
-    "station_maintenance",
-    "construction_expenses",
-    "federation_expenses"
+    r"\sproduced_energy",
+    r"\s(ship|army|colony|station)_maintenance",
+    r"\s(construction|trade|federation)_expenses"
 }
 # 3.0.*
 targets3 = {
     r"surveyed\s*=\s*\{(\s*)set_surveyed": r"set_surveyed = {\1surveyed",
-    r"planet = solar_system\n?": "",
-    "has_completed_special_project": "has_completed_special_project_in_log",
-    "has_failed_special_project": "has_failed_special_project_in_log",
-    r"species = last_created(\s)": r"species = last_created_species\1",
-    r"owner = last_created(\s)": r"owner = last_created_country\1",
+    r"has_completed_special_project\s+": "has_completed_special_project_in_log ",
+    r"has_failed_special_project\s+": "has_failed_special_project_in_log ",
+    r"species\s*=\s*last_created(\s)": r"species = last_created_species\1",
+    r"owner\s*=\s*last_created(\s)": r"owner = last_created_country\1",
     r"(any|every|random)_pop\s*=": r"\1_owned_pop =",
     r"(\s)(any|every|random)_planet\s*=": r"\1\2_galaxy_planet =",
     r"(\s)(any|every|random)_ship\s*=": r"\1\2_galaxy_fleet =",
@@ -37,14 +33,15 @@ targets3 = {
     r"(any|every|random)_war_defender\s*=": r"\1_defender =",
     r"(any|every|random)_war_attacker\s*=": r"\1_attacker =",
     r"(any|every|random|count)_recruited_leader\s*=": r"\1_owned_leader =",
-    "count_planets ": "count_galaxy_planet ",
-    "count_ships ": "count_galaxy_fleet ",
-    "count(_owned)?_pops ": "count_owned_pop ",
-    "count_owned_ships ": "count_owned_ship ",
-    # "any_ship_in_system": "any_fleet_in_system",
-    r"spawn_megastructure = \{\n([^{}]+)location": r"spawn_megastructure = {\n\1planet",
+    r"count_planets\s*": "count_galaxy_planet ",
+    r"count_ships\s*": "count_galaxy_fleet ",
+    r"count(_owned)?_pops\s*": "count_owned_pop ",
+    r"count_owned_ships\s*": "count_owned_ship ",
+    # "any_ship_in_system": "any_fleet_in_system", # works only sure for single size fleets
+    r"spawn_megastructure\s*=\s*\{([^{}]+)location\s*=": r"spawn_megastructure = {\1planet =",
+    r"\s+planet =\s*(solar_system|planet)\s*\n?": "", # REMOVE
     r"any_system_within_border\s*=\s*\{[\s\n]*any_system_planet\s*=": "any_planet_within_border =",
-    r"is_country_type = default\s+has_monthly_income = \{ resource = (\w+) value <=? \d": r"no_resource_for_component = { RESOURCE = \1",
+    r"is_country_type\s*=\s*default\s+has_monthly_income = \{ resource = (\w+) value <=? \d": r"no_resource_for_component = { RESOURCE = \1",
     r"([^\._])(?:space_)?owner\s*=\s*\{\s*is_(?:same_empire|country|same_value)\s*=\s*(\w+)\s*\}": r"\1is_owned_by = \2",
 }
 
@@ -113,10 +110,10 @@ def modfix(file_list):
                     # for line in file_contents:
                     # print(line)
                     for rt in removedTargets:
-                        if rt in line:
+                        rt = re.search(rt, line)
+                        if rt:
                             print("WARNING outdated removed trigger: %s in line %i file %s" % (
-                                rt, i, basename))
-                            # changed = True
+                                rt.group(0), i, basename))
                     for pattern, repl in targets3.items():
                         if re.search(pattern, line):
                             # , count=0, flags=0
